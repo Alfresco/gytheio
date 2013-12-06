2d96e76356a6fb747372523cc287b50b7558afbd
rgauss <rgauss@rgauss.com>
2013-12-06 09:22:04 -0500

    Removed Alfresco-specific messaging and transformation examples

--- alfresco-transform-repo-amp/src/main/java/org/alfresco/repo/content/transform/TransformationOptionsMessageConverter.java
+++
@@ -1,9 +0,0 @@
-package org.alfresco.repo.content.transform;
-
-public interface TransformationOptionsMessageConverter
-{
-
-    public org.gytheio.content.transform.options.TransformationOptions 
-        convert(org.alfresco.service.cmr.repository.TransformationOptions options);
-    
-}
