5edd4c573e4ba5d3ee51c0761be8a6ac150e5e07
rgauss <rgauss@rgauss.com>
2014-05-25 15:14:04 -0400

    - License headers and notices - Addition of apache-rat-plugin

--- gytheio-content-handlers/gytheio-content-handler-webdav/src/main/java/org/gytheio/content/handler/webdav/WebDavContentReferenceHandlerImpl.java
+++ gytheio-content-handlers/gytheio-content-handler-webdav/src/main/java/org/gytheio/content/handler/webdav/WebDavContentReferenceHandlerImpl.java
@@ -1,20 +1,20 @@
 /*
- * Copyright (C) 2005-2013 Alfresco Software Limited.
+ * Copyright (C) 2005-2014 Alfresco Software Limited.
  *
- * This file is part of Alfresco
+ * This file is part of Gytheio
  *
- * Alfresco is free software: you can redistribute it and/or modify
+ * Gytheio is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Alfresco is distributed in the hope that it will be useful,
+ * Gytheio is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ * along with Gytheio. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.gytheio.content.handler.webdav;
 
