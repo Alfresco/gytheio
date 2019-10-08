ad9195600811aa9ea5dae0694a7e7ce36fdb54b1
aftene <iulian.aftene@ness.com>
2019-10-08 11:07:18 +0300

    MM-800: Transition build from Bamboo to Travis-CI (#126)
    * MM-800 - Gytheio: Update to build on Travis-CI (rather than Bamboo)
     - add build.sh, init.sh and whitesource.sh scripts
     - add travis.yml
     - add settings.xml
    
    * MM-800 - Gytheio: Update to build on Travis-CI (rather than Bamboo)
     - add release.sh
     - update init, build and whitesource.sh
    
    * MM-800 - Gytheio: Update to build on Travis-CI (rather than Bamboo)
     - update pom.xml, settings.xml and release.sh
    
    * MM-800 - Gytheio: Update to build on Travis-CI (rather than Bamboo)
     - remove skipTests
    
    * MM-800 - Gytheio: Update to build on Travis-CI (rather than Bamboo)
     - update gytheio branches in build.sh
    
    * MM-800: Gytheio test dependencies and WhiteSource
    
    * MM-800 - Gytheio: Update to build on Travis-CI (rather than Bamboo)
     - add build and release readme
    
    * MM-800 - Gytheio: Update to build on Travis-CI (rather than Bamboo)
     - update release readme
    
    * MM-800: Fix link in Readme.md
    
    (cherry picked from commit 05c09a0)

---
+++ _ci/build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+echo "=========================== Starting Build&Test Script ==========================="
+PS4="\[\e[35m\]+ \[\e[m\]"
+set -vex
+pushd "$(dirname "${BASH_SOURCE[0]}")/../"
+
+# If the branch is "master" and the commit is not a Pull Request then deploy the JAR SNAPSHOT artifacts
+[ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ] && DEPLOY="deploy" || DEPLOY="install"
+
+mvn -B -U clean source:jar source:test-jar ${DEPLOY} \
+    -DaltDeploymentRepository=alfresco-internal::default::https://artifacts.alfresco.com/nexus/content/repositories/snapshots/
+
+popd
+set +vex
+echo "=========================== Finishing Build&Test Script =========================="
+
