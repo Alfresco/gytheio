6eccd93894f253b798885be1e71ddc7d718bcc33
Damian.Ujma@hyland.com <Damian.Ujma@hyland.com>
2023-02-14 15:24:06 +0100

    ACS-4501 Fix branch filter + release.sh

--- _ci/release.sh
+++ _ci/release.sh
@@ -5,7 +5,7 @@ PS4="\[\e[35m\]+ \[\e[m\]"
 set -vex
 pushd "$(dirname "${BASH_SOURCE[0]}")/../"
 
-# GHA CI runner work on DETACHED HEAD, so we need to checkout the release branch
+# Use full history for release
 git checkout -B "${BRANCH_NAME}"
 
 # Run the release plugin - with "[skip ci]" in the release commit message
@@ -14,7 +14,7 @@ mvn -B \
     "-Darguments=-DskipTests -Dmaven.javadoc.skip -Dadditionalparam=-Xdoclint:none -Dbuildnumber=${BUILD_NUMBER}" \
     release:clean release:prepare release:perform \
     -DscmCommentPrefix="[maven-release-plugin][skip ci] " \
-    -Dusername=alfresco-build \
+    -Dusername=${GIT_USERNAME} \
     -Dpassword=${GIT_PASSWORD}
 
 popd
